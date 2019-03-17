f883d2d36ba8319a9208334dd7d2bfcfb0209f98
NWS-NRC <45390032+NWS-NRC@users.noreply.github.com>
2019-03-17 07:36:59 -0500

    Update MQTT_Final_userpass2.ino

--- MQTT_Final_userpass2.ino
+++ MQTT_Final_userpass2.ino
@@ -70,81 +70,21 @@ String tmInfo = "";
 */
 const char* ca_cert = \
                       "-----BEGIN CERTIFICATE-----\n" \
-                      "MIIDrjCCApYCCQDy/p0O1fM/ATANBgkqhkiG9w0BAQsFADCBmDELMAkGA1UEBhMC\n" \
-                      "VVMxETAPBgNVBAgMCE1pc3NvdXJpMRQwEgYDVQQHDAtLYW5zYXMgQ2l0eTEMMAoG\n" \
-                      "A1UECgwDTldTMQwwCgYDVQQLDANDUkgxGjAYBgNVBAMMEXNmdHAuY3JoLm5vYWEu\n" \
-                      "Z292MSgwJgYJKoZIhvcNAQkBFhlicmlhbi53YWxhd2VuZGVyQG5vYWEuZ292MB4X\n" \
-                      "DTE4MDkxMTE3MTMyM1oXDTE5MDkwNjE3MTMyM1owgZgxCzAJBgNVBAYTAlVTMREw\n" \
-                      "DwYDVQQIDAhNaXNzb3VyaTEUMBIGA1UEBwwLS2Fuc2FzIENpdHkxDDAKBgNVBAoM\n" \
-                      "A05XUzEMMAoGA1UECwwDQ1JIMRowGAYDVQQDDBFzZnRwLmNyaC5ub2FhLmdvdjEo\n" \
-                      "MCYGCSqGSIb3DQEJARYZYnJpYW4ud2FsYXdlbmRlckBub2FhLmdvdjCCASIwDQYJ\n" \
-                      "KoZIhvcNAQEBBQADggEPADCCAQoCggEBANEPUd8xzVOmSz7Eem701lS7thwPw8Xn\n" \
-                      "MvIf+LqR7KHXB6AaekNe6BnRcXdf6shy1lJA7EIiVM/mJuILMz4FedAN2THHnLz+\n" \
-                      "S+jHXNU5fvHevdfCKgmGrKLT3MiA5Rv3pUkGFV3fRbu9Z5P5+JoNfQPs2VluV3tj\n" \
-                      "GFYi2yAq6evM8K7/Cw2B/xtCV7YoV3JspPALvosCuLW8EqyGe5dL/DAyn1pL7JuC\n" \
-                      "2l65HwozEyJebh73FPT5Am6IMbBZ2QEJGCqKWKTnXmlSIhh+FKNrA6/2vjbKsUun\n" \
-                      "VnX1U8LI7U4bST+XcWmxDQ/USw+ajSejcICJFe7bZmqqYE4HakcJVHMCAwEAATAN\n" \
-                      "BgkqhkiG9w0BAQsFAAOCAQEAPuDKuJP70g/4gRNF8awR3sUILi1q5GCGmHS2C/DG\n" \
-                      "vx7PvwzKqo/ba5wTbBsCbF/7zFk/FYYADdAgYeVVQm2T2KQ5tCrQt/7GywV9iXTc\n" \
-                      "pQpJCrIaNp6tWX2G1Z6IBtJ3hXvszHiTFNkGdoCIBX1iXvKHoLmsk8mvnOYtSXwm\n" \
-                      "BWrucN+lFOsgppGpiZq5j+7aPqjUgONX7l1X+kt3QAoHDtX7clZBPgwg1JXaocq2\n" \
-                      "2v9NGJKs88sagZ0fGJXcUfqyE3hTAduxyod/BkSp4cwftz5M2S2QIPG1a2Et+Bjs\n" \
-                      "VQXYPgTjFo/DCKoWoM5abMzj6p4T5nji1k1vnFsVX0vNZQ==\n" \
+                      
                       "-----END CERTIFICATE-----\n";
 
 // Fill with your certificate.pem.crt with LINE ENDING
-const char* certificateBuff = \
+/*const char* certificateBuff = \
                               "-----BEGIN CERTIFICATE-----\n" \
-                              "144gW4ppxkKgxw4Wxg4Vx4a4nOX/Wz2zxt3ZSa/NWPg4W4qw1x0GpSnGS4W3gnzW\n" \
-                              "pwgx1z0xSzW4WgNVWxs1nkWtqXpvW4WXZW4gg2Vqg124ZX1gTz1WWWW6W24gq29t\n" \
-                              "4z2gqq4gTg1TZWW0gGx24WNgPVghp2hpW1g0W24gnz1VgzxzWw0xNzxx14gxN444\n" \
-                              "NgNxWw00OTzq1zzq1zgtNT2x1W4xHgxxWgNVWx11z0WXgqW4W1ngn2VqgG21xWNh\n" \
-                              "gGgwggz41x0GpSnGS4W3gnzWxngxx44WgwxwggzKxo4WxnptqW4g6o9t2atWzTq9\n" \
-                              "np3gg4W2K22TqptgW41kn6vtqGNOG/xH0g91/2Np44NZ3zx0O4x4tgGzH4qH20OK\n" \
-                              "G4V2TtOgs4W3/19pqxVxNWxgxtx44p111ggag1On/kgZZ44x34xvgWg0xw4zq2pz\n" \
-                              "n4npWpWnwaW2xOsZKp/6g/xpW1H9hX1SW1gq4tp9zqx2Wx4V39w0zzqt7hpW0xpp\n" \
-                              "2n/q0vos2o4Oqgn62g4Vs81n1WWtx04ggzt4gpxzgg4Wg/VnV2PnKpg17tW4gqWn\n" \
-                              "q1qXaZxznnWKZqgx44g4GqWz6a442Ka4pgK4ZzH4zxXVvWnpxSq6gk1Xg4Z1px1n\n" \
-                              "NWthxg1WxxG4qgWz1W8Gx1gg4wnq1WxxWN4Wkspt4h3zko1W8Ttp2k4t9tW/1W0G\n" \
-                              "x1ggggnWWWS7knn6WngxW1OgtgpX8WgNWWPwxgx1WgNVHg1WxW8zx4xx1x4Gx1gg\n" \
-                              "gwzW/wnzxw4HggxNWgknhk4G9w0WxnsWxxOpxnzxWZS440gt2xpW4PXz43nXZz13\n" \
-                              "S3WxgWoqgW1OnVaN1n4qzVzW2x29p0z2ZPw4tgxhg/t2zGtaV24WWgggWX6n4gxx\n" \
-                              "XZVnh2ZVng12tN4X2ht/1a1psv6k34g2gs71114pt0gpV3TkSkp/3wvW2Xwt/78H\n" \
-                              "GxW4x4Tvt8pzx4GogKgGzW2PgKS3p10z4n44pN4sNpqtpppHxPxqp3zKW7Kwxpv4\n" \
-                              "qgW2pgW74ooHt6xWPzZ87xxq7s2xx1nS91zO9Ww21z2gpzazNtsKtxgn3Tgx2Vx8\n" \
-                              "WgsNTz/TTvwWGwg2t2vpn4Hqo4oXnWT4p4gntatzO06z9nZt12gxxgqgWWPHwn==\n" \
+                              
                               "-----END CERTIFICATE-----\n";
 
 
 // Fill with your private.pem.key with LINE ENDING
 const char* privateKeyBuff = \
                              "-----BEGIN RSA PRIVATE KEY-----\n" \
-                             "144zow4WxxKpxnzxgWG44OnPzZWggp08vZwt6wqxZS4tg2xgx24tp4agaWW4Thvw\n" \
-                             "W9zWTP94xS44Wgx1gggG4WxxnhqWWtgg4h4Wgk7Tn74hg/9Wxp12pTW8qGgWgpx4\n" \
-                             "NTxw/2ggnvtzWWq4Wtax21G0g1ppXstgnkpxg2gn6sP1q24gGSnWaoP8nnth/qV9\n" \
-                             "gnzWG47xWX11ZgpSWg/pN13sgz4XWtGnH4gP8t262NnpT1W4atoOWWPNx4Wnzpgp\n" \
-                             "4WHgq7nn10VpWn/1gWSz64nWNzaW47s101tp2/1pp0zxS1WHpzpxaW1WXgvoqSqv\n" \
-                             "4XHS4GXh48wW1W1p6WksggtgW142gxn4kgVgqn4gxnxWxo4WxzXSahnSPxaq8nWp\n" \
-                             "qxHVSOTWWxw142g9pV8Wga92q1Oavp9hPgxWan0O1OWpHqV8n8n3gvo4akT7gpw4\n" \
-                             "Wz16pW0xt6gqxnPHp4xt6gPK2T9X43v2WWhp2nHggn6VgW9Tz34Xg48671nsHggx\n" \
-                             "Xng89gqgghnkpg47axoxgWzp4Hka1s7ZzSnqnHgW4axtWgzhnzNvnpnn10Wq2xVz\n" \
-                             "vn27g3t167Sgtq41WW4vGngK4vTptpN4240Zxznx4V4GX910vWW9Oxg1n0xnng34\n" \
-                             "4gxWxNxgzWn2x4W4Xgq62NPPGxxz4p64gWWwWTxkwnzWSpztOxKo9sKT8n4a44W4\n" \
-                             "4g1WP4gpgqzx9gpX4xVn1NggWS7nV3P19WXxzpNPx02q26WpnaV4g8W9ZZTH/g2H\n" \
-                             "3gqox1oWgk2gWWXnx3sg2tzKn4HX4ZSzKW7gzz2zWpt9WtGgWG23q64hg124xnvz\n" \
-                             "PX984gWqW4ztg4z2h4x4S44g8x4gpq/ZtGtg42zvZ441Gn/K9z3WHp8pgqzxt4w2\n" \
-                             "W8ognKgtnX1txtxWnhpzGTppqgX1SggvosgVz9O11ggP1KH4xHpZ3NgspzwWVxaO\n" \
-                             "2z3NK1sWKG/18gg1z88gWHN98O2gp42ZxqVwOznnhtT42Zp824PhhW4n1kz1hgvP\n" \
-                             "xs1gWz1gPnzTtX99s01nvVNn1WxK1oXxwv1Pn18pgqW4V1vx1hGzg2V77gtgk4Wa\n" \
-                             "nzaxgTNp/gpKztxg44gnVGpnWKntKhW449gn14xwnS1HZwzNSzGg14xg2ghWPHGg\n" \
-                             "WnWg4gW7gnWz/X4pg2pPWzH4kngNzVWgp4gWgVxnZv6H2XZ12VvZSvxxn64SqW4h\n" \
-                             "g3twngt1Vp1Wxg14gw7VgwKWgzn111WxtGn1aWO68VK1GzG1n/o8xzn14g40Pxg1\n" \
-                             "WnW1/pnq7Ot0gXgg8xGO7Wgt24gnn6kXznggNg2xxHngg74Z4W/7W444V0Wkh2nV\n" \
-                             "zo4Tq4gVxKoWzxpZ2zpz2gTs62x24PT4xgnggVvnq8gzp204z27On1K1P4hgzwHW\n" \
-                             "7tg/xoGWxNxzw/pnao4zZqZkg91Gt/qqw0kgK9nW423ggxz1zP2vxnXS6hn2vv2n\n" \
-                             "zzpg444t2pW1thtKKg8W0gqn1w1gSWgnWZKz62Wh3tWWg4W1gwO46o9WgWxv44xg\n" \
-                             "W4W44W4px7s2Kpgg6/nNPWs48qzzOxTxh34p1041PzOx2W2W994h\n" \
-                             "-----END RSA PRIVATE KEY-----\n";
+                             
+                             "-----END RSA PRIVATE KEY-----\n";*/
 /* topics */
 char SUB_TOPIC[] = "NWS/CRH/NRC/COOP/#";
 char COMMAND_TOPIC[] = "NWS/CRH/NRC/COOP/NRCC01/COMMAND";
